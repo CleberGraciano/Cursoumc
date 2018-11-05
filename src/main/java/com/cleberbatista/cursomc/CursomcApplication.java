8f4d0c1a169c534366cff255ca2d5830e3237744
unknown <cleber_batista2010@hotmail.com>
2018-11-05 20:32:46 -0200

    Criando end point teste

--- src/main/java/com/cleberbatista/cursomc/CursomcApplication.java
+++ src/main/java/com/cleberbatista/cursomc/CursomcApplication.java
@@ -1,4 +1,4 @@
-package com.cleberbatista.cursomc;
+ package com.cleberbatista.cursomc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
