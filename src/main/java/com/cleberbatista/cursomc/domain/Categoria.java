066ec21b51642af07f0576628ae1527a0dbd7ab2
unknown <cleber_batista2010@hotmail.com>
2018-11-05 21:20:36 -0200

    Configurando Banco de Dados H2 salvando em arquivo e em memoria, e criação de dados a partir dos objetos

--- src/main/java/com/cleberbatista/cursomc/domain/Categoria.java
+++ src/main/java/com/cleberbatista/cursomc/domain/Categoria.java
@@ -2,11 +2,19 @@ package com.cleberbatista.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable{
 	
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
