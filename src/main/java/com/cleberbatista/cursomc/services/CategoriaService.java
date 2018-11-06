b9039d25755a82b0792ed850eb61353ecefa836d
unknown <cleber_batista2010@hotmail.com>
2018-11-05 23:12:44 -0200

    Adicionando import da classe

--- src/main/java/com/cleberbatista/cursomc/services/CategoriaService.java
+++ src/main/java/com/cleberbatista/cursomc/services/CategoriaService.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.cleberbatista.cursomc.services.exceptions.ObjectNotFoundException;
 
 import com.cleberbatista.cursomc.domain.Categoria;
 import com.cleberbatista.cursomc.repositories.CategoriaRepository;
@@ -18,7 +19,7 @@ public class CategoriaService {
 
 		Optional<Categoria> obj = repo.findById(id); 
 
-		return obj.orElseThrow(() -> new com.cleberbatista.cursomc.services.exceptions.ObjectNotFoundException(
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
