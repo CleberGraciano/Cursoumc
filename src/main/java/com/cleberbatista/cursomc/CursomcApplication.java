5f239f5f5571cbc22b01d4f689758a817b243012
unknown <cleber_batista2010@hotmail.com>
2018-12-18 22:29:40 -0200

    Estado e Cidade

--- src/main/java/com/cleberbatista/cursomc/CursomcApplication.java
+++ src/main/java/com/cleberbatista/cursomc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.cleberbatista.cursomc.domain.Categoria;
+import com.cleberbatista.cursomc.domain.Cidade;
+import com.cleberbatista.cursomc.domain.Estado;
 import com.cleberbatista.cursomc.domain.Produto;
 import com.cleberbatista.cursomc.repositories.CategoriaRepository;
+import com.cleberbatista.cursomc.repositories.CidadeRepository;
+import com.cleberbatista.cursomc.repositories.EstadoRepository;
 import com.cleberbatista.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -43,11 +53,24 @@ public class CursomcApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
+			
 		
 		repo.saveAll(Arrays.asList(cat1, cat2, cat3));
 		
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
+		Estado est1 = new Estado (null, "Minas Gerais");
+		Estado est2 = new Estado (null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campias", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 	}
 	
 	
