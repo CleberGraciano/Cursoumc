c673214849590e3eed24e47399f1f8f84648ae7b
unknown <cleber_batista2010@hotmail.com>
2018-12-20 02:00:04 -0200

    Validação sintatica com Bean validation

--- src/main/java/com/cleberbatista/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/cleberbatista/cursomc/dto/CategoriaDTO.java
@@ -2,6 +2,11 @@ package com.cleberbatista.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+
+import org.hibernate.validator.constraints.Length;
+
 import com.cleberbatista.cursomc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable{
@@ -10,6 +15,9 @@ public class CategoriaDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 	
 	public CategoriaDTO() {}
--- src/main/java/com/cleberbatista/cursomc/services/CategoriaService.java
+++ src/main/java/com/cleberbatista/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.cleberbatista.cursomc.domain.Categoria;
+import com.cleberbatista.cursomc.dto.CategoriaDTO;
 import com.cleberbatista.cursomc.repositories.CategoriaRepository;
 import com.cleberbatista.cursomc.services.exceptions.DataIntegrityException;
 import com.cleberbatista.cursomc.services.exceptions.ObjectNotFoundException;
@@ -63,6 +64,10 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 
 }
