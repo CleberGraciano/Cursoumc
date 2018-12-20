5a2e9c20688b666b7644ae27e3d2149666ce65da
unknown <cleber_batista2010@hotmail.com>
2018-12-20 01:32:10 -0200

    Paginação com parametros opcionais na requisição

--- src/main/java/com/cleberbatista/cursomc/CursomcApplication.java
+++ src/main/java/com/cleberbatista/cursomc/CursomcApplication.java
@@ -71,7 +71,12 @@ public class CursomcApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		Categoria cat3 = new Categoria(null, "Empresa");
-
+		Categoria cat4 = new Categoria(null, "Cama mesa e banho");
+		Categoria cat5 = new Categoria(null, "Eletronicos");
+		Categoria cat6 = new Categoria(null, "Jardinagem");
+		Categoria cat7 = new Categoria(null, "Decoração");
+		Categoria cat8 = new Categoria(null, "Perfumaria");
+		
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
 		Produto p3 = new Produto(null, "Mouse", 80.00);
@@ -83,7 +88,7 @@ public class CursomcApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 
-		repo.saveAll(Arrays.asList(cat1, cat2, cat3));
+		repo.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7, cat8));
 
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 
--- src/main/java/com/cleberbatista/cursomc/services/CategoriaService.java
+++ src/main/java/com/cleberbatista/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.cleberbatista.cursomc.domain.Categoria;
@@ -18,15 +21,15 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 
-	public Categoria find (Integer id) {
+	public Categoria find(Integer id) {
 
-		Optional<Categoria> obj = repo.findById(id); 
+		Optional<Categoria> obj = repo.findById(id);
 
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
-	public Categoria insert (Categoria obj) {
+
+	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
@@ -34,25 +37,32 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
-		
+
 	}
 
 	public void delete(Integer id) {
 		find(id);
 		try {
-			
-		repo.deleteById(id);
-		
-		}catch (DataIntegrityViolationException e) {
-			
+
+			repo.deleteById(id);
+
+		} catch (DataIntegrityViolationException e) {
+
 			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos");
 		}
-		
+
 	}
 
 	public List<Categoria> findAll() {
-		
+
 		return repo.findAll();
+	}
+
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+
+		return repo.findAll(pageRequest);
 	}
 
 }
