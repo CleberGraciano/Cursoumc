534c643e3bdd54d882100fc227825f92cebc8444
cleberGraciano <cleber_batista2010@hotmail.com>
2019-04-08 23:35:05 -0300

    Validação customizada de CPF ou CNPJ na inserção de Clientes

--- src/main/java/com/cleberbatista/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/cleberbatista/cursomc/dto/ClienteNewDTO.java
@@ -2,19 +2,45 @@ package com.cleberbatista.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.cleberbatista.cursomc.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
+	
+	@NotEmpty(message="Preenchimento Obrigatório")
+	@Length(min=5, max=120, message="O tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
+	
+	@NotEmpty(message="Preenchimento Obrigatório")
 	private String email;
+	
+	@NotEmpty(message="Preenchimento Obrigatório")
 	private String cpfOuCnpj;
+	
+	
 	private Integer tipo;
+	
+	@NotEmpty(message="Preenchimento Obrigatório")
 	private String logradouro;
+	
+	@NotEmpty(message="Preenchimento Obrigatório")
 	private String numero;
+	
 	private String complemento;
+	
 	private String bairro;
+	
+	@NotEmpty(message="Preenchimento Obrigatório")
 	private String cep;
 	
+	@NotEmpty(message="Preenchimento Obrigatório")
 	private String telefone1;
 	private String telefone2;
 	private String telefone3;
---
+++ src/main/java/com/cleberbatista/cursomc/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,38 @@
+package com.cleberbatista.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.cleberbatista.cursomc.domain.enums.TipoCliente;
+import com.cleberbatista.cursomc.dto.ClienteNewDTO;
+import com.cleberbatista.cursomc.resources.exceptions.FieldMessage;
+import com.cleberbatista.cursomc.services.validation.utils.BR;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+		
+		if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			 list.add(new FieldMessage("cpfOuCnpj", "CPF Inválido"));
+		}
+		
+		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			 list.add(new FieldMessage("cpfOuCnpj", "CNPJ Inválido"));
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
